1c89d03d7dd990bb8389b379ebe02b3a70a256e0
rameshbommidi <rameshbommidi125@gmail.com>
2023-12-27 03:54:59 +0000

    done

---
+++ src/components/BannerCardItem/index.js
@@ -0,0 +1,18 @@
+// Write your code here.
+const BannerCardItem = props => {
+  const {objectDetails, key} = props
+  const {headerText, description, className} = objectDetails
+
+  return (
+    <li key={key} className={className}>
+      <div className="content">
+        <h1 className="heading">{headerText}</h1>
+        <p className="paragraph">{description}</p>
+        <button className="buttons" type="button">
+          Show more
+        </button>
+      </div>
+    </li>
+  )
+}
+export default BannerCardItem
